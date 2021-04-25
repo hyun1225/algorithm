db34dc0faa4440967c0444eeecfb474745f04ecf
hyun1225 <lhma78@naver.com>
2021-04-25 19:31:30 +0900

    2021.04.25.일

---
+++ Algorithm/src/baekJoon/이분탐색/백준_10815_숫자카드.java
@@ -0,0 +1,71 @@
+package baekJoon.이분탐색;
+import java.util.*;
+
+
+
+
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Scanner;
+//a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+//b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+//g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+//var = sc.next();                            // 문자열 1개 입력받는 예제
+//AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+import java.io.*;
+
+public class 백준_10815_숫자카드 {
+	
+
+	public static int N,M;
+	public static int[] nums;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.setIn(new FileInputStream("res/백준input_10815.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		nums = new int[N];
+		
+		for(int i = 0 ; i < N ; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(nums);
+		
+		M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < M ; i ++) {
+			int a = Integer.parseInt(st.nextToken());
+			if(binarySearch(a)) {
+				sb.append("1").append(" ");
+			}else {
+				sb.append("0").append(" ");
+			}
+		}
+		
+		System.out.println(sb);
+		
+	}
+
+	private static boolean binarySearch(int findValue) {
+		int leftIndex = 0;
+		int rightIndex = N-1;
+		while(leftIndex <= rightIndex) {
+			int midIndex = (leftIndex + rightIndex) / 2;
+			int midValue = nums[midIndex];
+			if( midValue > findValue) {
+				rightIndex = midIndex -1;
+			}else if(midValue < findValue) {
+				leftIndex = midIndex +1;
+			}else {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
+
